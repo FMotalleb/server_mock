8238cea6c19830af08833eb25df8d9397d2aff5b
FMotalleb <fmotalleb@gmail.com>
2022-10-24 11:03:27 +0330

    init

---
+++ bin/server_mock.dart
@@ -0,0 +1,5 @@
+import 'package:server_mock/server_mock.dart' as server_mock;
+
+void main(List<String> arguments) {
+  print('Hello world: ${server_mock.calculate()}!');
+}
---
+++ test/server_mock_test.dart
@@ -0,0 +1,8 @@
+import 'package:server_mock/server_mock.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
